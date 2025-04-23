c359b79751d009e220ac59df8a0219dcd03cbf64
NSTechBytes <nstechbytes@gmail.com>
2025-04-23 18:30:31 +0500

    Read Flexes Path

--- Library/ConfigFile.js
+++ Library/ConfigFile.js
@@ -1,21 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 
-let Logging = 0;
-let DarkMode = 0;
-
 function getIniValue(keyName) {
     const sectionName = "DeskFlex";
-    const filePath = process.env.APPDATA + "\\DeskFlex\\DeskFlex.ini";
+    const filePath = path.join(process.env.APPDATA, "DeskFlex", "DeskFlex.ini");
     const data = fs.readFileSync(filePath, 'utf-8');
     const result = {};
     let currentSection = null;
     const lines = data.split('\n');
     lines.forEach(line => {
         line = line.trim();
-        if (line === '' || line.startsWith(';') || line.startsWith('#')) {
-            return;
-        }
+        if (line === '' || line.startsWith(';') || line.startsWith('#')) return;
+
         if (line.startsWith('[') && line.endsWith(']')) {
             currentSection = line.slice(1, -1).trim();
             result[currentSection] = {};
@@ -27,16 +23,24 @@ function getIniValue(keyName) {
         }
     });
 
-    if (result[sectionName] && result[sectionName][keyName]) {
-        return result[sectionName][keyName];
-    } else {
-        return null;
-    }
+    return result[sectionName]?.[keyName] ?? null;
 }
 
-// Assign values
-Logging = getIniValue('Logging') || 0;
-DarkMode = getIniValue('DarkMode') || 0;
+function getLogging() {
+    return parseInt(getIniValue('Logging')) || 0;
+}
 
-console.log('Logging is', Logging); 
-console.log('DarkMode is', DarkMode);
+function getDarkMode() {
+    return parseInt(getIniValue('DarkMode')) || 0;
+}
+
+function getFlexesPath() {
+    return getIniValue('FlexesPath');
+}
+
+module.exports = {
+    getIniValue,
+    getLogging,
+    getDarkMode,
+    getFlexesPath
+};
--- Library/DeskFlex.js
+++ Library/DeskFlex.js
@@ -1,7 +1,23 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const { getLogging, getDarkMode,getFlexesPath } = require('./ConfigFile');
 
+let Logging = getLogging();
+let DarkMode = getDarkMode();
 
+if (Logging === 1) {
+  console.log('Logging is Enabled.');
+} else {
+  console.log('Logging is Disabled.');
+}
+
+if (DarkMode === 1) {
+  console.log('DarkMode is Enabled.');
+} else {
+  console.log('DarkMode is Disabled.');
+}
+
+console.log(`Flexes Path is: ${getFlexesPath()}`)
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
@@ -14,7 +30,6 @@ function createWindow() {
   });
   win.setMenu(null);
   win.loadFile(path.join(__dirname, 'MainWindow', 'index.html'));
-  
 }
 
 app.whenReady().then(createWindow);
